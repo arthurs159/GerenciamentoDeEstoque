90fee747d15660ffdae43c1c06103ae499c41395
Arthur Scarpati <arthur_159@hotmail.com>
2021-12-21 16:42:33 -0300

    Fix: Company tests and added product tests

--- Back-end/src/test/java/stock/controllers/CompanyControllerTests.java
+++ Back-end/src/test/java/stock/controllers/CompanyControllerTests.java
@@ -1,6 +1,5 @@
 package stock.controllers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,21 +23,11 @@ public class CompanyControllerTests {
 	public void findAllShouldReturnAllResourcesSortedByName() throws Exception {
 		
 		ResultActions resultado =
-				mockMvc.perform(get("/companies")
+				mockMvc.perform(get("/company")
 						.contentType(MediaType.APPLICATION_JSON));
 		
 		resultado.andExpect(status().isOk());
-		resultado.andExpect(jsonPath("$[0].name").value("Bom Garoto"));
-		resultado.andExpect(jsonPath("$[1].name").value("IndosFarm"));
-		resultado.andExpect(jsonPath("$[2].name").value("Mercado Quero"));
-		
-		resultado.andExpect(jsonPath("&[0].password").value("12345"));
-		resultado.andExpect(jsonPath("&[1].password").value("123456"));
-		resultado.andExpect(jsonPath("&[2].password").value("1234567"));
-		
-		resultado.andExpect(jsonPath("&[0].cnpj").value("20061986000117"));
-		resultado.andExpect(jsonPath("&[1].cnpj").value("77360719000168"));
-		resultado.andExpect(jsonPath("&[2].cnpj").value("20177053000190"));
+		resultado.andExpect(jsonPath("$[0].name").value("Mercado Ponto Certo"));
 		
 	}
 	
@@ -55,31 +44,7 @@ public class CompanyControllerTests {
 		resultado.andExpect(jsonPath("$.name").isNotEmpty());
 		resultado.andExpect(jsonPath("$.password").isNotEmpty());
 		resultado.andExpect(jsonPath("$.cnpj").isNotEmpty());
-		
 	}
 	
-	@Test
-	public void deleteShouldReturnoNoContentOnAnExistingId() throws Exception {
-		
-		Long existingId = 2L;
-		
-		ResultActions resultado =
-				mockMvc.perform(delete("/companies/{id}", existingId));
-		
-		resultado.andExpect(status().isNoContent());
-	}
-	
-	public void deleteShoulReturnNotFoundWhenIdNotExists() throws Exception {
-		
-		Long nonExistentId = 10L;
-		
-		ResultActions resultado = 
-				mockMvc.perform(delete("/companies/{id}", nonExistentId));
-		
-		resultado.andExpect(status().isNotFound());
-		
-	}
-	
-	
-		
+
 }
---
+++ Back-end/src/test/java/stock/controllers/ProductControllerTests.java
@@ -0,0 +1,109 @@
+package stock.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	@Test
+	public void findAllShouldReturnAllResourcesSortedByName() throws Exception {
+		
+		ResultActions resultado =
+				mockMvc.perform(get("/products")
+						.contentType(MediaType.APPLICATION_JSON));
+		
+		resultado.andExpect(status().isOk());
+		resultado.andExpect(jsonPath("$[0].name").value("Barra de chocolate"));
+		resultado.andExpect(jsonPath("$[1].name").value("Queijo coalho"));
+		resultado.andExpect(jsonPath("$[2].name").value("Geleia de morango"));
+		
+	}
+	
+	@Test
+	public void findByIdShouldReturnTheCompanyWithThisId() throws Exception {
+		
+		Long existingId = 1L;
+		
+		ResultActions resultado = 
+				mockMvc.perform(get("companies/{id}", existingId));
+		
+		resultado.andExpect(status().isOk());
+		resultado.andExpect(jsonPath("$.id").value(existingId));
+		resultado.andExpect(jsonPath("$.name").isNotEmpty());
+		resultado.andExpect(jsonPath("$.quantity").isNotEmpty());
+		resultado.andExpect(jsonPath("$.brand").isNotEmpty());
+	}
+	
+	@Test
+	void insertShouldCreateNewProduct() throws Exception {
+		ProductDTO dto = new ProductDTO(null, "Molho de tomate", 10, "Heinz");
+		String jsonBody = objectMapper.writeValueAsString(dto);
+		
+		ResultActions resultado =
+				mockMvc.perform(post("/products")
+						.content(jsonBody)
+						.contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON));
+		
+		resultado.andExpect(status().isCreated());
+					
+	}
+	
+	@Test
+	public void findByIdShouldReturnNotFoundWhenCompanyDoesNotExists() throws Exception {
+		
+		Long nonExistingId = 10L;
+		
+		ResultActions resultado =
+				mockMvc.perform(get("/products/{id}", nonExistingId));
+		
+		resultado.andExpect(status().isNotFound());
+	}
+	
+	
+	@Test
+	public void deleteShouldReturnoNoContentOnAnExistingId() throws Exception {
+		
+		Long existingId = 2L;
+		
+		ResultActions resultado =
+				mockMvc.perform(delete("/products/{id}", existingId));
+		
+		resultado.andExpect(status().isNoContent());
+	}
+	
+	public void deleteShoulReturnNotFoundWhenIdNotExists() throws Exception {
+		
+		Long nonExistentId = 10L;
+		
+		ResultActions resultado = 
+				mockMvc.perform(delete("/products/{id}", nonExistentId));
+		
+		resultado.andExpect(status().isNotFound());
+		
+	}
+	
+	
+	
+}
