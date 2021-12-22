59b568bef75cfcd479be02f19ddbf0faf01666c8
Arthur Scarpati <arthur_159@hotmail.com>
2021-12-22 16:27:38 -0300

    findByName, insert, delete, update service from Product

--- Back-end/src/main/java/stock/repositories/ProductRepository.java
+++ Back-end/src/main/java/stock/repositories/ProductRepository.java
@@ -1,10 +1,12 @@
 package stock.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import stock.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	
-
+	Optional<Product> findByName(String name);
 }
--- Back-end/src/main/java/stock/services/ProductService.java
+++ Back-end/src/main/java/stock/services/ProductService.java
@@ -1,9 +1,12 @@
 package stock.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import stock.entities.Product;
 import stock.entities.dto.ProductDTO;
@@ -15,10 +18,42 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repository;
 	
+	@Transactional(readOnly = true)
 	public Page<ProductDTO> listAll(Pageable pageable) {
 		Page<Product> page = repository.findAll(pageable);
 		return page.map(x -> new ProductDTO(x));
 	}
 	
+	@Transactional(readOnly = true)
+	public ProductDTO findByName(String name) {
+		Optional<Product> product = repository.findByName(name);
+		Product entity = product.orElseThrow();
+		return new ProductDTO(entity);
+	}
 	
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+		Product prod = new Product();
+		toDto(dto, prod);
+		prod = repository.save(prod);
+		return new ProductDTO(prod);
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
+	public ProductDTO update(Long id, ProductDTO dto) {
+		Product prod = repository.getOne(id);
+		toDto(dto, prod);
+		prod  = repository.save(prod);
+		return new ProductDTO(prod);
+	}
+	
+	
+	private void toDto(ProductDTO dto, Product entity) {
+		entity.setName(dto.getName());
+		entity.setQuantity(dto.getQuantity());
+		entity.setBrand(dto.getBrand());
+	}
 }
