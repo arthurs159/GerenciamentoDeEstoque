2d20e5f63a315b7ce4f0e0c1c15ed9f70dcc9b1e
Arthur Scarpati <arthur_159@hotmail.com>
2021-12-23 10:19:42 -0300

    Remove product

--- Back-end/src/main/java/stock/services/ProductService.java
+++ Back-end/src/main/java/stock/services/ProductService.java
@@ -18,20 +18,20 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
-	
+
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> listAll(Pageable pageable) {
 		Page<Product> page = repository.findAll(pageable);
 		return page.map(x -> new ProductDTO(x));
 	}
-	
+
 	@Transactional(readOnly = true)
 	public ProductDTO findByName(String name) {
 		Optional<Product> product = repository.findByName(name);
 		Product entity = product.orElseThrow();
 		return new ProductDTO(entity);
 	}
-	
+
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product prod = new Product();
@@ -39,35 +39,46 @@ public class ProductService {
 		prod = repository.save(prod);
 		return new ProductDTO(prod);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
-	
+
 	@Transactional
 	public ProductDTO update(Long id, ProductDTO dto) {
 		Product prod = repository.getOne(id);
 		toDto(dto, prod);
-		prod  = repository.save(prod);
+		prod = repository.save(prod);
 		return new ProductDTO(prod);
 	}
-	
-	
+
 	private void toDto(ProductDTO dto, Product entity) {
 		entity.setName(dto.getName());
 		entity.setQuantity(dto.getQuantity());
 		entity.setBrand(dto.getBrand());
 	}
-	
-	
+
 	public ProductDTO addProduct(Long id, int quantity) throws ProductNotFoundException {
 		Product addProduct = repository.getOne(id);
-		
+
 		int quantityToAdd = quantity + addProduct.getQuantity();
 		addProduct.setQuantity(quantityToAdd);
-		
+
 		Product productAdded = repository.save(addProduct);
 		return new ProductDTO(productAdded);
 	}
-	
+
+	public ProductDTO RemoveProduct(Long id, int quantity) throws ProductNotFoundException {
+		Product removeProduct = repository.getOne(id);
+
+		int quantityToRemove = quantity - removeProduct.getQuantity();
+
+		if (quantityToRemove > 0) {
+			removeProduct.setQuantity(quantityToRemove);
+			Product productAdded = repository.save(removeProduct);
+			return new ProductDTO(productAdded);
+		}
+		throw new ProductNotFoundException("O Produto não pode ter quantidade negativa");
+	}
+
 }
