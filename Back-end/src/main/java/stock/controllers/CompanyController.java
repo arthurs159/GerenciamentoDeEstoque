184554c983e7f02882d032f860b33eb84d2ab084
Arthur Scarpati <arthur_159@hotmail.com>
2021-12-23 13:59:44 -0300

    Fix: entities DB and H2 database

---
+++ Back-end/src/main/java/stock/controllers/CompanyController.java
@@ -0,0 +1,26 @@
+package stock.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import stock.entities.dto.CompanyDTO;
+import stock.services.CompanyService;
+
+@RestController
+@RequestMapping(value = "/company")
+public class CompanyController {
+
+	@Autowired
+	private CompanyService service;
+	
+	@GetMapping
+	ResponseEntity<List<CompanyDTO>> listCompany(){
+		List<CompanyDTO> company = service.listCompany();
+		return ResponseEntity.ok().body(company);
+	}
+}
--- Back-end/src/main/java/stock/entities/Product.java
+++ Back-end/src/main/java/stock/entities/Product.java
@@ -1,6 +1,12 @@
 package stock.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,8 +16,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Product {
+@Table(name = "tb_product")
+public class Product implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private int quantity;
