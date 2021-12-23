bb1714c842e5b5f5a2b6b121f22b9d3a3bd29e46
Arthur Scarpati <arthur_159@hotmail.com>
2021-12-23 14:33:18 -0300

    Add some data on data.sql and List all, GetByName on Product Controller

--- Back-end/src/main/java/stock/controllers/ProductController.java
+++ Back-end/src/main/java/stock/controllers/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +18,17 @@ public class ProductController {
 
 	@Autowired
 	private ProductService service;
-	
+
 	@GetMapping
-	ResponseEntity<Page<ProductDTO>> listAllPaged(Pageable pageable){
+	ResponseEntity<Page<ProductDTO>> listAllPaged(Pageable pageable) {
 		Page<ProductDTO> dto = service.listAll(pageable);
 		return ResponseEntity.ok().body(dto);
 	}
-}
+
+	@GetMapping(value = "/{name}")
+	ResponseEntity<ProductDTO> findByName(@PathVariable String name) {
+		ProductDTO dto = service.findByName(name);
+		return ResponseEntity.ok().body(dto);	
+		}
+
+	}
---
+++ Back-end/src/main/resources/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO TB_COMPANY (name, password, cnpj) VALUES ('Supermercado Do Bom', '1234567' , '35349428000129');
+
+INSERT INTO TB_PRODUCT (name, quantity, brand) VALUES ('Molho de tomate', 10, 'Pomarola');
+INSERT INTO TB_PRODUCT (name, quantity, brand) VALUES ('Azeitona', 5, 'Hammer');
+INSERT INTO TB_PRODUCT (name, quantity, brand) VALUES ('Sabão em pó', 15, 'Omo');
+INSERT INTO TB_PRODUCT (name, quantity, brand) VALUES ('Agua mineral', 55, 'Ambev');
+INSERT INTO TB_PRODUCT (name, quantity, brand) VALUES ('Macarrão', 40, 'Knor');
+INSERT INTO TB_PRODUCT (name, quantity, brand) VALUES ('Amaciante', 10, 'Omo');
+INSERT INTO TB_PRODUCT (name, quantity, brand) VALUES ('Champignon', 16, 'Hammer');
+INSERT INTO TB_PRODUCT (name, quantity, brand) VALUES ('Caldo de carne', 10, 'Knor');
+INSERT INTO TB_PRODUCT (name, quantity, brand) VALUES ('Cerveja Brahma 600ml', 60, 'Ambev');
+INSERT INTO TB_PRODUCT (name, quantity, brand) VALUES ('Cerveja Skol 1L', 45, 'Ambev');
+INSERT INTO TB_PRODUCT (name, quantity, brand) VALUES ('Molho de tomate sabor especial', 10, 'Pomarola');
+INSERT INTO TB_PRODUCT (name, quantity, brand) VALUES ('Ketchup', 14, 'Pomarola');
+INSERT INTO TB_PRODUCT (name, quantity, brand) VALUES ('Cerveja Antartica', 49, 'Ambev');
+INSERT INTO TB_PRODUCT (name, quantity, brand) VALUES ('Milho verde conserva', 18, 'Hammer');
