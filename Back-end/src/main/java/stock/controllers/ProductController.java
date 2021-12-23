6f60c37538a8204fbaf14676fd173520b9a1b162
Arthur Scarpati <arthur_159@hotmail.com>
2021-12-23 14:59:24 -0300

    Insert product

--- Back-end/src/main/java/stock/controllers/ProductController.java
+++ Back-end/src/main/java/stock/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package stock.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import stock.entities.dto.ProductDTO;
 import stock.services.ProductService;
@@ -37,6 +41,13 @@ public class ProductController {
 	ResponseEntity<List<ProductDTO>> findByBrand(@PathVariable String brand) {
 		List<ProductDTO> dto = service.findByBrand(brand);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	ResponseEntity<ProductDTO> insert (@RequestBody ProductDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 
 }
